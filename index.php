6dc9fe564b436d07a6ef8798b59b90022763f8df
TamasH <unconfigured@null.spigotmc.org>
2023-03-23 13:58:22 +0100

    html

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Rente berekenen<title>
+        <link>
+    </head>
+    <body>
+        <h1>Bereken uw rente.</h1>
+        <?php echo "uhhh bruh?";?>
+    </body>
+</html>
