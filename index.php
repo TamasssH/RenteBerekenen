40f18cee73949dbf41549312be8060204bc38541
TamasH <unconfigured@null.spigotmc.org>
2023-03-28 11:27:57 +0200

    checks for input + display error message

--- index.php
+++ index.php
@@ -2,41 +2,53 @@
 <html>
     <head>
         <title>Rente berekenen</title>
-        <link>
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
         <?php
-
-            if (isset($_POST["submit"])) {
-                
+            $errorChoice = $errorval = "";
+            $option = "";
+            $ingVal = $rntPercentage = 0;
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $output = 0.000;
                 //check for input in the radio buttons & number input fields.
-                if (empty($_POST["radio1"]) && empty($_POST["radio2"])) {
-                    echo "Je moet een keuze maken.<br>";
+                if (empty($_POST["option"])) {
+                    $errorChoice = "Je moet een keuze maken.<br>";
+                }else {
+                    $option = $_POST["option"];
                 }
                 if (empty($_POST["ingVal"]) || empty($_POST["rntPercentage"])) {
-                    echo "Vul een waarde in.";
+                        $errorVal = "Vul een waarde in.";
                 } else {
                     $ingVal = $_POST["ingVal"];
                     $rntPercentage = $_POST["rntPercentage"];
                     //bereken hoeveel rente
-                    $rntPercentage /= 100;
-                    $output = $ingVal * (1+$rntPercentage)+pow(10,10);
+                    if ($option == "option1") {
+                        for ($i=0; $i<10;$i++) {
+                            $output = $ingVal * (1+$rntPercentage/100)^$i;
+                        }
+                    }else {
+                        //andere rente berekening hier.
+                    }
                 }
             }
         ?>
         <h1>Bereken uw rente.</h1>
         <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+            <p class="error"><?php echo $errorval; ?></p>
             <label>Ingelegd bedrag: </label>
-            <input name="ingVal" type="number" min="0" ><br><br>
+            <input name="ingVal" type="number" min="0" value="<?php echo $ingVal ?>"><br><br>
+
             <label>Rente percentage (in %): </label>
-            <input name="rntPercentage" type="number" step="0.0001"><br><br>
-            <input name="radio1" type="radio"><label>Eindbedrag na 10 jaar</label><br><br>
-            <input name="radio2" type="radio"><label>Eindbedrag verdubbeld</label><br><br>
+            <input name="rntPercentage" type="number" step="0.0001" value="<?php echo $rntPercentage ?>"><br><br>
+
+            <p class="error"><?php echo $errorChoice; ?></p>
+            <input name="option" type="radio" value="option1" <?php if(isset($option) && $option == "option1") echo 'checked="checked"'; ?> ><label>Eindbedrag na 10 jaar</label><br><br>
+            <input name="option" type="radio" value="option2" <?php if(isset($option) && $option == "option2") echo 'checked="checked"'; ?>><label>Eindbedrag verdubbeld</label><br><br>
+
             <button name="submit">Bereken</button><br><br>
             <input name="endVal" type="number" readonly value="<?php echo $output ?>">
             
         </form>
-       
     </body>
 </html>
