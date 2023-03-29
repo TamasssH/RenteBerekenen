59e8d15e1ac8cda2b60a61baed2cf5d144a03494
TamasH <unconfigured@null.spigotmc.org>
2023-03-29 15:34:42 +0200

    wewe

--- index.php
+++ index.php
@@ -6,11 +6,10 @@
     </head>
     <body>
         <?php
-            $errorChoice = $errorval = "";
-            $option = "";
-            $ingVal = $rntPercentage = 0;
+            $errorChoice = $errorval = $option = "";
+            $ingVal = $rntPercentage = 0; 
+            $output = 0.000;
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $output = 0.000;
                 //check for input in the radio buttons & number input fields.
                 if (empty($_POST["option"])) {
                     $errorChoice = "Je moet een keuze maken.<br>";
@@ -24,11 +23,12 @@
                     $rntPercentage = $_POST["rntPercentage"];
                     //bereken hoeveel rente
                     if ($option == "option1") {
-                        for ($i=0; $i<10;$i++) {
-                            $output = $ingVal * (1+$rntPercentage/100)^$i;
+                        for ($i=1; $i<=10;$i++) {
+                            $output = $ingVal * ($i+$rntPercentage/100);
+                            echo "<table>".$output."</table>";
                         }
                     }else if ($option == "option2") {
-                        $output = $ingVal * (1+$rntPercentage/100);
+                        $output = 72/$rntPercentage;
                     }
                 }
             }
@@ -39,7 +39,7 @@
             <label>Ingelegd bedrag: </label>
             <input name="ingVal" type="number" min="0" value="<?php echo $ingVal ?>"><br><br>
 
-            <label>Rente percentage (in %): </label>
+            <label>Rente percentage: </label>
             <input name="rntPercentage" type="number" step="0.0001" value="<?php echo $rntPercentage ?>"><br><br>
 
             <p class="error"><?php echo $errorChoice; ?></p>
@@ -47,7 +47,7 @@
             <input name="option" type="radio" value="option2" <?php if(isset($option) && $option == "option2") echo 'checked="checked"'; ?>><label>Eindbedrag verdubbeld</label><br><br>
 
             <button name="submit">Bereken</button><br><br>
-            <input name="endVal" type="number" readonly value="<?php echo $output ?>">
+            <input name="endVal" type="number" readonly value="<?php echo "<table>".$output."</table>" ?>">
             
         </form>
     </body>
