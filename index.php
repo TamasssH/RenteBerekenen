19e62e9a790ccbfea0e16bd4cae4797692fd71d4
TamasH <unconfigured@null.spigotmc.org>
2023-04-12 13:52:41 +0200

    code clean
    De code is een klein beetje meer clean.

--- index.php
+++ index.php
@@ -9,23 +9,18 @@
              $errorChoice = $errorval = $option = "";
              $ingVal = $rntPercentage = 0; 
              $output = 0.000;
-        
         ?>
         <h1>Bereken uw rente.</h1>
         <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
             <p class="error"><?php echo $errorval; ?></p>
             <label>Ingelegd bedrag: </label>
             <input name="ingVal" type="number" min="0" value="<?php echo $ingVal ?>"><br><br>
-
             <label>Rente percentage: </label>
             <input name="rntPercentage" type="number" step="0.0001" value="<?php echo $rntPercentage ?>"><br><br>
-
             <p class="error"><?php echo $errorChoice; ?></p>
             <input name="option" type="radio" value="option1" <?php if(isset($option) && $option == "option1") echo 'checked="checked"'; ?> ><label>Eindbedrag na 10 jaar</label><br><br>
             <input name="option" type="radio" value="option2" <?php if(isset($option) && $option == "option2") echo 'checked="checked"'; ?>><label>Eindbedrag verdubbeld</label><br><br>
-
             <button type="submit" name="submit">Bereken</button><br><br>
-            
         </form>
         <?php
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -50,15 +45,11 @@
                         echo "</table>";
                     }else if ($option == "option2") {
                         echo "<table>";
-                        $i=0;       
-                        $output = $ingVal;         
-                        while ($output/2 != $ingVal) {
-                            $i++;
+                        for ($i=1;$i<=2;$i++) {
                             $output += $ingVal * (1+$rntPercentage/100);
                             echo "<tr>"."<td> jaar: ".$i." </td>"."<td>€".$output."</td>"."</tr>";
                         }
                         echo "</table>";
-                        
                     }
                 }
             }
